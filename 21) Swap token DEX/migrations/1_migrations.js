4374be4b99895fe60f71307a7387cd4095f91ecb
SwpnilPndey <swpnil.pndey@gmail.com>
2023-04-01 16:30:24 +0530

    Deleted Swap Token DEX smart contract

--- 21) Swap token DEX/migrations/1_migrations.js
+++
@@ -1,9 +0,0 @@
-const swap=artifacts.require("DEX")
-
-module.exports=function(deployer) {
-    const tokenAddress="0x64311F21D04534189d60848D8aDfA5Fc07E7B79e";
-    const BigNumber = require('bignumber.js');
-    const rate = new BigNumber('1000000000000000000');
-    deployer.deploy(swap,tokenAddress,rate);
-}
-
