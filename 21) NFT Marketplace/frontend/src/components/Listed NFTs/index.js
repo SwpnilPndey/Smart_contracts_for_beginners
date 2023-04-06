e5787f5b00eb8226c2f7eb6e4f1eafdd6cb622e7
SwpnilPndey <swpnil.pndey@gmail.com>
2023-04-06 23:07:55 +0530

    Started with front end

---
+++ 21) NFT Marketplace/frontend/src/components/Listed NFTs/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './listednft.css'
+
+const ListedNFTs = () => {
+  return (
+    <div className='listed-nfts'>ListedNFTs</div>
+  )
+}
+
+export default ListedNFTs
